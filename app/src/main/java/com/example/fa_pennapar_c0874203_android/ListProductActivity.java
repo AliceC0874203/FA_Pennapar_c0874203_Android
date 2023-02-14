77f3e8dbc52f4028e1326f795f30a9d28a477ad0
Alice <c0874203@mylambton.ca>
2023-02-13 21:50:46 -0500

    change name
    change name

--- app/src/main/java/com/example/fa_pennapar_c0874203_android/ListProductActivity.java
+++ app/src/main/java/com/example/fa_pennapar_c0874203_android/ListProductActivity.java
@@ -42,6 +42,7 @@ public class ListProductActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_product);
+        setTitle("Product List");
 
         setRecyclerViewAndAdapter();
         enableProductViewModel();
